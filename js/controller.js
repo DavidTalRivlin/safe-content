2a7e3bd8ca50150dac1c04ea968f6a29ca5835b4
DavidTalRivlin <david.dave.tal@gmail.com>
2023-11-19 15:32:05 +0200

    Change stucture

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,6 @@
 'use strict'
 
+
 function onInit() {
       var logedInUser = loadFromStorage('logedInUser')
 
@@ -10,6 +11,58 @@ function onInit() {
 
 
 }
+
+
+
+function onInitAdmin() {
+      var logedInUser = loadFromStorage('logedInUser')
+
+      if (!logedInUser || !logedInUser.isAdmin) {
+            removeFromStorage('logedInUser')
+            goToLoginPage()
+      }
+      renderTable()
+}
+
+
+
+function renderTable() {
+      const elTable = document.querySelector('.users-table')
+
+      const keys = Object.keys(gUsers[0])
+
+      var strHtml =
+
+            `<tr>
+      <th>${keys[0]}</th>
+      <th>${keys[1]}</th>
+      <th>${keys[2]}</th>
+      <th>${keys[3]}</th>
+      <th>${keys[4]}</th>
+      </tr>`
+
+      strHtml += gUsers.map((user) => {
+            return `<tr>
+      <td>${user.id}</td>
+      <td>${user.username}</td>
+      <td>${user.password}</td>
+      <td>${getDateAndTime(user.lastLoginTime)}</td>
+      <td>${user.isAdmin}</td>
+      </tr>`
+      }).join('')
+
+      elTable.innerHTML = strHtml
+}
+
+
+function onSetSortBy(elSelect) {
+      setSortBy(elSelect.value)
+      renderTable()
+}
+
+
+
+
 
 function checkLogin(ev) {
       ev.preventDefault()
